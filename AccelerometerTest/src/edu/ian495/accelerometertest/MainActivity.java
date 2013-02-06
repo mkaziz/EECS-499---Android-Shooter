32041be01964b7a59b7bce64a74966d1b6d3a9d1
Khalid <kaziz@u.northwestern.edu>
2013-02-06 13:57:32 -0600

    Initial test accelerometer project added that displays curr acc values onscreen

---
+++ AccelerometerTest/src/edu/ian495/accelerometertest/MainActivity.java
@@ -0,0 +1,71 @@
+package edu.ian495.accelerometertest;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private long lastUpdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lastUpdate = System.currentTimeMillis();
+        TextView textView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // TODO Auto-generated method stub
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            float[] values = event.values;
+            // Movement
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(Float.toString(x) + " " + Float.toString(y) + " "
+                    + Float.toString(z));
+        }
+    }
+}
