ff1dd417b3addb1bdcac40ed42b8d7fc1bc2bace
Khalid <kaziz@u.northwestern.edu>
2013-02-13 18:38:19 -0600

    Added Physics engine to repo

--- AndEngineTest/src/edu/ian/andenginetest/MainActivity.java
+++ AndEngineTest/src/edu/ian/andenginetest/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.ian.andenginetest;
 
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,34 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(
+            OnCreateResourcesCallback pOnCreateResourcesCallback)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene,
+            OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+        // TODO Auto-generated method stub
+
     }
 
 }
