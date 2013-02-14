22d3cc06e83717e3ea641acaa3e26992cd335bc0
Khalid <kaziz@u.northwestern.edu>
2013-02-13 19:53:20 -0600

    Now showing coloured screen

--- AndEngineTest/src/edu/ian/andenginetest/MainActivity.java
+++ AndEngineTest/src/edu/ian/andenginetest/MainActivity.java
@@ -1,53 +1,65 @@
 package edu.ian.andenginetest;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
-import org.andengine.ui.activity.BaseGameActivity;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.ui.activity.BaseActivity;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-import android.os.Bundle;
-import android.view.Menu;
+import android.graphics.Typeface;
 
-public class MainActivity extends BaseGameActivity {
+public class MainActivity extends SimpleBaseGameActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    static final int CAMERA_WIDTH = 800;
+    static final int CAMERA_HEIGHT = 480;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public Font mFont;
+    public Camera mCamera;
+
+    public Scene mCurrentScene;
+    public static BaseActivity instance;
+
+    public static BaseActivity getSharedInstance() {
+        return instance;
     }
 
     @Override
     public EngineOptions onCreateEngineOptions() {
         // TODO Auto-generated method stub
-        return null;
+        instance = this;
+        mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR,
+                new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
     }
 
     @Override
-    public void onCreateResources(
-            OnCreateResourcesCallback pOnCreateResourcesCallback)
-            throws Exception {
-        // TODO Auto-generated method stub
+    protected void onCreateResources() {
+        mFont = FontFactory.create(this.getFontManager(),
+                this.getTextureManager(), 256, 256,
+                Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32);
 
+        mFont.load();
     }
 
     @Override
-    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
-            throws Exception {
+    protected Scene onCreateScene() {
         // TODO Auto-generated method stub
-
+        mEngine.registerUpdateHandler(new FPSLogger());
+        mCurrentScene = new Scene();
+        mCurrentScene.setBackground(new Background(0.09804f, 0.0274f, 0.8f));
+        return mCurrentScene;
     }
 
-    @Override
-    public void onPopulateScene(Scene pScene,
-            OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-        // TODO Auto-generated method stub
-
+    public void setCurrentScene(Scene scene) {
+        mCurrentScene = scene;
+        getEngine().setScene(mCurrentScene);
     }
 
 }
