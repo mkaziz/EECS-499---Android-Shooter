aaebc8621546b888c7de054ea4746a01283a7ac9
Khalid <kaziz@u.northwestern.edu>
2013-03-06 20:43:46 -0600

    Getting ship moving onscreen w/ Physics

--- AndEngineTest/src/edu/ian/andenginetest/GameScene.java
+++ AndEngineTest/src/edu/ian/andenginetest/GameScene.java
@@ -3,7 +3,10 @@ package edu.ian.andenginetest;
 import org.andengine.engine.camera.hud.HUD;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.texture.region.ITextureRegion;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,6 +14,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 import edu.ian.andenginetest.SceneManager.SceneType;
 
@@ -18,12 +23,15 @@ public class GameScene extends BaseScene implements SensorEventListener {
 
     private Sprite backgroundSprite;
     private Sprite shipSprite;
+    private Body ship;
 
     private HUD gameHud;
     private PhysicsWorld physicsWorld;
 
     SensorManager sensorManager;
     Sensor accelerometer;
+
+    Vector2 currentAcceleration = new Vector2();
 
     public GameScene() {
         this.createScene();
@@ -47,14 +55,36 @@ public class GameScene extends BaseScene implements SensorEventListener {
 
     private void createShip() {
         AssetManager am = AssetManager.getInstance();
-
-        shipSprite = new Sprite(0, 0, am.getTextureRegion("gfx/ship.png"),
+        ITextureRegion shipTextureRegion = am.getTextureRegion("gfx/ship.png");
+        shipSprite = new Sprite(0, MainActivity.CAMERA_HEIGHT
+                - shipTextureRegion.getHeight(), shipTextureRegion,
                 mainActivity.getVertexBufferObjectManager());
+
+        ship = PhysicsFactory.createBoxBody(physicsWorld, shipSprite,
+                BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 1));
+        ship.setUserData(ship);
+        ship.setFixedRotation(true);
+
+        physicsWorld.registerPhysicsConnector(new PhysicsConnector(shipSprite,
+                ship, true, false) {
+
+            @Override
+            public void onUpdate(float pSecondsElapsed) {
+                super.onUpdate(pSecondsElapsed);
+                mainActivity.mCamera.onUpdate(0.1f);
+
+                ship.setLinearDamping(10.0f);
+                // ship.applyForce(currentAcceleration, new Vector2(0, 0));
+                ship.setLinearVelocity(currentAcceleration);
+
+            }
+        });
+
         this.attachChild(shipSprite);
     }
 
     private void createPhysics() {
-        physicsWorld = new FixedStepPhysicsWorld(60, new Vector2(0, -10), false);
+        physicsWorld = new FixedStepPhysicsWorld(60, new Vector2(0, 0), false);
         registerUpdateHandler(physicsWorld);
     }
 
@@ -62,8 +92,8 @@ public class GameScene extends BaseScene implements SensorEventListener {
     public void createScene() {
 
         createBackground();
-        createShip();
         createPhysics();
+        createShip();
 
     }
 
@@ -96,12 +126,32 @@ public class GameScene extends BaseScene implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         // TODO Auto-generated method stub
+
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
-            shipSprite.setX(shipSprite.getX() + (int) event.values[1]);
-            shipSprite.setY(shipSprite.getY() + (int) event.values[0]);
+            // Vector2 newAcceleration = currentAcceleration;
+            //
+            // if ((Math.abs((int) event.values[1] - (int)
+            // currentAcceleration.x)) > 1)
+            // newAcceleration.set((int) event.values[1],
+            // (int) currentAcceleration.y);
+            //
+            // if ((Math.abs((int) event.values[0] - (int)
+            // currentAcceleration.y)) > 1) {
+            // newAcceleration.set(newAcceleration.x, (int) event.values[0]);
+            // }
+            currentAcceleration.set(event.values[1], event.values[0])
+                    .mul(1.75f);
+            //
+            // if (newX > shipSprite.getWidth()
+            // || newX < mainActivity.CAMERA_WIDTH - shipSprite.getWidth())
+            // shipSprite.setX(newX);
+            // if (newY > mainActivity.CAMERA_HEIGHT / 2
+            // || newY < mainActivity.CAMERA_HEIGHT
+            // - shipSprite.getHeight())
+            // shipSprite.setY(newY);
 
         }
-    }
 
+    }
 }
